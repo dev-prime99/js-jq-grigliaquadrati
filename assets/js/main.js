af2054bd3c6a6526c05c81a7d6e9828b35e6a6f7
dev-prime99 <prime99@icloud.com>
2019-11-06 17:57:22 +0100

    bonus completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,35 @@
 
-var pod, pariodispari;
+var pod, pariodispari, rossi, verdi;
 
 var contatore = 0;
 
-var i = 0;
+rossi = 0;
+
+verdi = 0;
+
+/*
+  //BONUS
+
+for (var i = 0; i < 8; i++) {
+  document.getElementById('cube').innerHTML += "<div class='flex riga'><a href='#'></a><a href='#'></a><a href='#'></a><a href='#'></a><a href='#'></a><a href='#'></a><a href='#'></a><a href='#'></a></div>"
+}
+*/
+
 
 $("a").click(function (){
 
   pod = Math.floor(Math.random() * 100) + 1;
   console.log(pod);
-  if (pod % 2 == 0 && i < 15) {
+  if (pod % 2 == 0 && rossi < 15) {
     console.log("pari");
-    $(this).css("background-color", "red");
-    i += 1;
+    $(this).addClass("no-active red"); //BONUS
+    rossi += 1;
+    $("#rossi").html(rossi);
   } else{
     console.log("dispari");
-    $(this).css("background-color", "green");
+    $(this).addClass("no-active green"); //BONUS
+    verdi += 1;
+    $("#verdi").html(verdi);
   }
-  console.log(i);
-  
+
 })
